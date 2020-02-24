a713effcaaee37bd5a8ed2aa1dfccb5dccab5fd1
Sultan Kenjeyev <ksula0155@gmail.com>
2020-02-24 16:59:40 +0000

    reduce waypoints

--- comp0037/comp0037_planner_controller/src/comp0037_planner_controller/controller_base.py
+++ comp0037/comp0037_planner_controller/src/comp0037_planner_controller/controller_base.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import rospy
-from geometry_msgs.msg  import Twist
-from geometry_msgs.msg  import Pose2D
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose2D
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
-from math import pow,atan2,sqrt,pi
+from math import pow, atan2, sqrt, pi
 from planned_path import PlannedPath
 import time
 import math
+import pickle as pkl
+import numpy as np
+
 
 # This is the base class of the controller which moves the robot to its goal.
 
@@ -34,7 +37,7 @@ class ControllerBase(object):
         # Store the occupany grid - used to transform from cell
         # coordinates to world driving coordinates.
         self.occupancyGrid = occupancyGrid
-        
+
         # This is the rate at which we broadcast updates to the simulator in Hz.
         self.rate = rospy.Rate(10)
 
@@ -56,7 +59,7 @@ class ControllerBase(object):
 
         position = odometryPose.position
         orientation = odometryPose.orientation
-        
+
         pose.x = position.x
         pose.y = position.y
         pose.theta = 2 * atan2(orientation.z, orientation.w)
@@ -67,7 +70,7 @@ class ControllerBase(object):
                 self.prevY = pose.y
                 self.prevTheta = pose.theta
                 self.firstTime = False
-            dDistance = sqrt((pose.x - self.prevX)**2+(pose.y - self.prevY)**2)
+            dDistance = sqrt((pose.x - self.prevX) ** 2 + (pose.y - self.prevY) ** 2)
             self.distanceTravelled += dDistance
             self.angleTurned += (abs(pose.theta - self.prevTheta))
             self.prevX = pose.x
@@ -94,9 +97,12 @@ class ControllerBase(object):
         self.plannerDrawer = plannerDrawer
         self.notTeleportGoal = True
 
-
         rospy.loginfo('Driving path to goal with ' + str(len(path.waypoints)) + ' waypoint(s)')
-        
+
+        with open('/home/ros_user/workspace/path_waypoints.pkl', 'wb') as fp:
+            pkl.dump(path.waypoints, fp)
+
+        print('waypoints dumped')
         # Drive to each waypoint in turn
         for waypointNumber in range(0, len(path.waypoints)):
             cell = path.waypoints[waypointNumber]
@@ -108,8 +114,29 @@ class ControllerBase(object):
                 break
 
         rospy.loginfo('Rotating to goal orientation (' + str(goalOrientation) + ')')
-        
+
         # Finish off by rotating the robot to the final configuration
         if rospy.is_shutdown() is False:
             self.rotateToGoalOrientation(goalOrientation)
- 
+
+    def reduceWaypoints(self, waypoints):
+        new_waypoints = []
+        i, j = 0, len(waypoints) - 1
+        xs = [cell.coords[0] for cell in waypoints]
+        ys = [cell.coords[1] for cell in waypoints]
+        thresh = 0.1
+        while i < j:
+            p, V = np.polyfit(xs[i:j], ys[i:j], 1, cov=True)
+            error = np.sqrt(V[1][1])
+            if error < thresh:
+                new_waypoints.append(waypoints[i])
+                new_waypoints.append(waypoints[j])
+                i = j
+                j = len(waypoints)
+            else:
+                j -= 1
+
+            print "x_1: {} +/- {}".format(p[0], np.sqrt(V[0][0]))
+            print "x_2: {} +/- {}".format(p[1], np.sqrt(V[1][1]))
+        print new_waypoints
+        return new_waypoints
